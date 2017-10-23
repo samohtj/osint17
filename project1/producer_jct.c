bd2f9d63974d2cc48b63c83f1b0859ecead6beca
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2017-10-23 11:31:41 -0400

    Moves some things to macros
    Simple one-liner functions changed to macros. No need for
    fancy shit. Also made a new header file for common variables.
    Added some more/better comments.

--- project1/producer_jct.c
+++ project1/producer_jct.c
@@ -1,14 +1,20 @@
+/* 
+ * Jonathan Thomas
+ * CEG 4350 Fall 2017
+ * Project 1: Producer / Consumer Problem
+ * Creates producer processes that add messages to a buffer in shared memory space.
+ */
+
 #include "producer_jct.h"
 
 /*
-    Boilerplate identifiying comment goes here
-
-*/
+ * Main entry point. Parses terminal arguments to determine what action to take.
+ */
 int main(int argc, char** argv) {
     switch(argc) {
     case 2:
         if (strcmp(argv[1], HELP_ARG) != 0) {
-	    improper_args(1);
+	    improper_args();
 	    return 1;
 	} else {
 	    /* User asks for help! */
@@ -18,19 +24,19 @@ int main(int argc, char** argv) {
     case 5:
 	if (check_args(argv[1], argv[2], argv[3]) != 0) {
 	    /* Invalid arguments. Yell at user! */
-	    improper_args(3);
+	    improper_args();
 	    return 1;
 	} else {
 	    /* Actually run the program */
 	    return 0;
 	}
     case 1:
-	/* Run the program with default arguments. */
+		/* Run the program with default arguments. */
 	return 0;
     default:
-	/* Improper number of arguments. Print proper useage to screen*/
-	improper_num_arguments();
-	return 1;
+		/* Improper number of arguments. Print proper useage to screen*/
+		improper_num_arguments();
+		return 1;
     }
 }
 
@@ -41,42 +47,20 @@ void produce() {
 }
 
 /*
- *   Improper or invalid arguments have been enetered Will print a message to
- *   the console specifying proper next steps.
- *
- *   @param count The number of arguments being passed. Valied values are 1 (help
- *	argument) or 3 (normal run). Helps discern what the user was tyring to do.
- */
-void improper_args(int count) {
-    if (count == 1) {
-	printf("Unknown argument. Run with %s for help\n", HELP_ARG);	
-    } else if (count == 3) {
-	printf("Improper arguments. Correct usage: producer_jct <BUFFER SIZE> <NUM PRODUCERS> <NUM MESSAGES>\n");
-    }
-
-}
-
-/*
- *   Invlalid quantity of arguments was entered. Yell at the user!
- */
-void improper_num_arguments() {
-    improper_args(3);
-}
-
-/*
  * Print basic help information to the console.
  */
 void print_help_info() {
     printf("PRODUCER PROCESS\n");
     printf("To use: ");
     printf("\tproducer_jct <BUFFER SIZE> <NUM PRODUCERS> <NUM MESSAGES>\n");
+	printf("\tBUFFER SIZE: ")
 }
 
 /*
  * Check the validity of the arguments entered. They should all be integers.
  *
  * @param a, b, c Strings parsed from the command line arguments.
- * @return 0 if arguments are valid, false otherwise.
+ * @return 0 if arguments are valid, 1 otherwise.
  */
 int check_args(char * a, char * b, char * c) {
    return 0; 
--- project1/producer_jct.h
+++ project1/producer_jct.h
@@ -1,10 +1,22 @@
 #include <string.h>
 #include <stdio.h>
 
+/*
+ * Terminal option to print help information.
+ */
 #define HELP_ARG "-h"
 
+/*
+ *   Improper or invalid arguments have been enetered. Will print a message to
+ *   the console specifying proper next steps.
+ */
+#define improper_args() printf("Improper arguments. Correct usage: producer_jct <BUFFER SIZE> <NUM PRODUCERS> <NUM MESSAGES>\n");
+
+/*
+ *   Invlalid quantity of arguments was entered. Yell at the user!
+ */
+#define improper_num_arguments() improper_args()
+
 void produce();
-void improper_num_arguments();
-void improper_args(int);
 void print_help_info();
 int check_args(char *, char *, char *);
