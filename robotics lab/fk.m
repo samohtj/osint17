5adf7f655170b02d0d91299ed0cfb9c78cf8d4f1
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2017-11-08 14:11:03 -0500

    Finished lab 6 for robotics
    Inverse kinematics are really hard yo

--- robotics lab/fk.m
+++ robotics lab/fk.m
@@ -3,12 +3,13 @@ function [X] = fk(motorAngles)
     L2 = 0.27;
     L3 = 0.02;
 
-    theta1 = motorAngles(1);
-    theta2 = 90 + motorAngles(2) - atan2d(L3,L2);
-    theta3 = motorAngles(3) - 90 + atan2d(L3,L2);
-    theta4 = 0 % it’s always zero since motor 4 is not connected to any link
+%     theta1 = motorAngles(1);
+%     theta2 = 90 + motorAngles(2) - atan2d(L3,L2);
+%     theta3 = motorAngles(3) - 90 + atan2d(L3,L2);
+%     theta4 = 0 % it’s always zero since motor 4 is not connected to any link
 
-    thetas = [theta1, theta2, theta3, theta4];
+%     thetas = [theta1, theta2, theta3, theta4];
+    thetas = [motorAngles(1), motorAngles(2), motorAngles(3),0];
 
     X = T4_0(thetas);
 	
--- robotics lab/ik.m
+++ robotics lab/ik.m
@@ -1,7 +1,7 @@
 function [theta1, theta2, theta3 ] = ik( X, Y, Z )
    L1 = 0.103;
    L2 = 0.27;
-   L3 = 0.02;
+   L3 = 0.038;
    L4 = 0.205;
    L = sqrt(L3^2 + L2^2);
    
@@ -10,27 +10,31 @@ function [theta1, theta2, theta3 ] = ik( X, Y, Z )
 %    L = 1.05;
    
    
-   if (X < 0)
-       theta1 = atan2d(Y, -X);
+
+   theta1 = atan2d(Y,X)
+   
+   b = ((X*cosd(theta1) + Y*sind(theta1))^2 + (Z-L1)^2 - L4^2 - L^2)/(2.0*L*L4);
+   theta3 = atan2d(-sqrt(1-b^2), b)
+   if (theta3 >= 90)
+       theta3 = atan2d(sqrt(1-b^2), b)
+   end
+   
+   a = Z - L1;
+   b = -(X*cosd(theta1) + Y*sind(theta1));
+   c = L4*sind(theta3);
+   
+   theta2a = atan2d(c, -sqrt(a^2 + b^2 - c^2)) - atan2d(a, b);
+   theta2b = atan2d(c, sqrt(a^2 + b^2 - c^2)) - atan2d(a, b);
+   
+   fkresult1 = fk([theta1, theta2a, theta3]);
+   fkresult2 = fk([theta1, theta2b, theta3]);
+   
+   in = [X;Y;Z;1];
+   if (abs(fkresult1 - in) > abs(fkresult2 - in))
+       theta2 = theta2b
    else
-       theta1 = atan2d(-Y, X);
-   end
-   
-   
-   b = ((X*cos(theta1) + Y*sin(theta1))^2 + (Z-L1)^2 - L^2 - L4^2)/(2*L*L4);
-   
-   theta3 = atan2d(-sqrt(1-b^2), b);
-   if (theta3 > 90 || theta3 < -90)
-       theta3 = atan2d(sqrt(1-b^2), b);
-   end
-   
-   a = (X*cos(theta1)) + (Y*sin(theta1));
-   c = (L4*cos(theta3)) + L;
-   
-   r = sqrt(a^2 + Z^2);
-   theta2 = atan2d(c, -sqrt(r^2 - c^2)) - atan2d(a, Z);
-   if (theta2 > 90 || theta2 < -90)
-       theta2 = atan2d(c, sqrt(r^2 - c^2)) - atan2d(a, Z);
+       theta2 = theta2a
    end
 end
+
 
